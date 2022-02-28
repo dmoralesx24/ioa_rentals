ad367a699053e3b3cf5cfc52dd0f9b6dff97dfb8
Verified ✔️ <dmoralesx24@gmail.com>
2022-02-27 19:36:12 -0500

    dependencies

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
+import { Link } from "react-router-dom";
 import './Navbar.css';
 
 const Navbar = () => (
     <Menu fixed="top" inverted color={"blue"} widths={3} className='nav-bar'> 
-      <Menu.Item link>
-        Home
+      
+      <Menu.Item href='/'>
+          Home
       </Menu.Item>
 
-      <Menu.Item link>
+      <Menu.Item href='/about'>
         About
       </Menu.Item>
 
-      <Menu.Item link>
-        Bookings
+      <Menu.Item href='/bookings'>
+          Bookings
       </Menu.Item>
     </Menu>
 )
---
+++ src/pages/Bookings/Booking.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Image } from 'semantic-ui-react';
+
+const Booking = () => {
+    return (
+        <Container textAlign='center'>
+            <h1 className='black-text'>Bookings Page</h1> 
+        </Container>
+    )
+}
+
+export default Booking
